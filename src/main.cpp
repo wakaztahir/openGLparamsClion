96202aed387d1269355c71396bb4ed3811c799f5
Waqas Tahir <wakaztahir@gmail.com>
2022-03-11 12:26:39 +0000

    triangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <GLFW/glfw3.h>
 
-int main()
-{
-    GLFWwindow* window;
+int main() {
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -10,8 +9,7 @@ int main()
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -20,10 +18,15 @@ int main()
     glfwMakeContextCurrent(window);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
