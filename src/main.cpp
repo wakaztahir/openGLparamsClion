6740909946c79bd2357e23c93a9aaf3e851a2be7
Waqas Tahir <wakaztahir@gmail.com>
2022-03-11 19:16:12 +0000

    added basic shader

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,89 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+static bool ShaderCompilationError(unsigned int type,int id) {
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)(_malloca(length * sizeof(char)));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << ((type == GL_VERTEX_SHADER) ? "Vertex" : "Fragment") << " shader" << std::endl;
+        std::cout << message << std::endl;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+struct ShaderProgramSource {
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filePath) {
+
+    std::ifstream stream(filePath);
+
+    enum class ShaderType {
+        None = -1,Vertex = 0,Fragment = 1
+	};
+
+    std::string line;
+    std::stringstream ss[2];
+	auto type = ShaderType::None;
+
+    while (getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::Vertex;
+            }
+            else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::Fragment;
+            }
+            else {
+                std::cout << "Invalid Shader Name" << std::endl;
+            }
+        }
+        else {
+            ss[(int)type] << line << '\n';
+        }
+    }
+
+    return { ss[0].str(),ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string &source) {
+    unsigned int id = glCreateShader(type);
+    const char *src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    if(ShaderCompilationError(type,id)) {
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main() {
     GLFWwindow *window;
@@ -24,19 +107,45 @@ int main() {
     glfwMakeContextCurrent(window);
 
     // Initialize Glew
-    if(glewInit() != GLEW_OK)
+    if (glewInit() != GLEW_OK)
         std::cout << "Error -> GLEW NOT OK !" << std::endl;
+
+    std::cout << "GL Version : " << glGetString(GL_VERSION) << std::endl;
+
+
+    float positions[] = {
+        -0.5f,-0.5f,
+        0.0f,0.5f,
+        0.5f,-0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,(const void*) 0);
+
+
+    auto shaderSource = ParseShader("res/shaders/Basic.shader");
+
+    std::cout << "Vertex :" << std::endl;
+    std::cout << shaderSource.VertexSource << std::endl;
+
+    std::cout << "Fragment : " << std::endl;
+    std::cout << shaderSource.FragmentSource << std::endl;
+
+    unsigned int shader = CreateShader(shaderSource.VertexSource,shaderSource.FragmentSource);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        // Drawing a triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
